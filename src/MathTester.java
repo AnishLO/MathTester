66555e4334f5d5898e79d83767ae5acfdf743196
Anish <Anish.lohar.2022@student.lhschools.org>
2019-09-16 09:54:54 -0400

    Added two more verbals

--- src/MathTester.java
+++ src/MathTester.java
@@ -3,6 +3,7 @@ public class MathTester {
     public static void main (String[] args){
       String firstWord;
       String secondWord;
+      int firstNumber,secondNumber;
 
       firstWord = "dog";
       secondWord = "house";
